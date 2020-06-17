426486a34ebd52065ac8c870916560c653ce0599
shaico <shaico92@gmail.com>
2020-06-17 18:51:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Person from './Person/Person';
+
+
+class App extends Component  {
+
+  
+state = {
+  changed : false,
+  persons: [
+      {name: 'makore', age :23},
+      {name: 'sha1', age :21},
+      {name: 'sha2', age :24},
+      {name: 'sha3', age :26}
+
+  ]
+
+
+}
+
+
+
+switchnNmeHandler = ()=>{
+  
+  
+ // dont do this!!!! this.state.persons[0].name= 'newName';
+  if (this.state.changed===false) {
+    this.setState({
+      changed : true,
+      persons: [
+      {name: 'Newmakore', age :23},
+      {name: 'Newsha1', age :21},
+      {name: 'Newsha2', age :24},
+      {name: 'Newsha3', age :26}
+    
+    ]})   
+  } else {
+    this.setState({
+      changed : false,
+      
+      persons: [
+      {name: 'makore', age :23},
+      {name: 'sha1', age :21},
+      {name: 'sha2', age :24},
+      {name: 'sha3', age :26}
+
+  ] })}
+ 
+
+
+
+
+  
+}
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button onClick={this.switchnNmeHandler}>Switch Name</button>
+<Person name= {this.state.persons[0].name} age ={this.state.persons[0].age}/>
+<Person name= {this.state.persons[1].name} age ={this.state.persons[1].age}/>
+<Person name= {this.state.persons[2].name} age ={this.state.persons[2].age}/>
+<Person name= {this.state.persons[3].name} age ={this.state.persons[3].age}>this is just a random content</Person>
+
+      </div>
   );
+  }
 }
 
 export default App;
