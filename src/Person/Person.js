b244b6343581d0fdc9aa4aab08e23074718b7d5f
shaico92 <shaico92@gmail.com>
2020-06-23 23:30:40 +0300

    change button to x

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 
+import  './Person.css'
+
+import xBtn from '../xButton.png'
 const Person = (props)=>{
 
     return(
 
       <div style={props.style} >
-
-    <p  onClick={props.click}>  i am {props.name} and i  am {props.age} years old</p>
+        <img onClick={props.click} className='btn' src={xBtn}></img>
+    <p  >  i am {props.name} and i  am {props.age} years old</p>
 
     <p>{props.children}</p>
     <input type='text' onChange={props.changed} value={props.name}></input>
